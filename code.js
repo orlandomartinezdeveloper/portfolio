505795966a694b25357910f90ae11762af98ce82
Orlando Martinez <omjuniormusic@gmail.com>
2021-04-15 19:26:49 -0300

    Trabalhando no JS

--- code.js
+++ code.js
@@ -1,8 +1,14 @@
-var nombre = document.querySelector("#nome");
-var correo = document.getElementById('email');
-var texto = document.getElementById('text');
-var boton = document.getElementById('buttonEnviar');
 function fechar() {
 	document.getElementById("check").checked = false;
 }
-
+var boton = document.getElementById('buttonEnviar').addEventListener('click', validar);
+function validar() {
+	let nombre = document.getElementById("nome");
+	let correo = document.getElementById('email');
+	let texto = document.getElementById('text');
+	if (nombre.value.trim() || correo.value.trim() || texto.value.trim()== '') {
+		document.getElementById('completaNome').style.display = "block";
+	} else {
+		document.getElementById('mensagemEnviado').style.display = "block";
+	}
+}
