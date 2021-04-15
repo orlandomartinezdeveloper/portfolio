a0d1f1a9770731b8dc49303bde069bb28e6d18c7
Orlando Martinez <omjuniormusic@gmail.com>
2021-04-15 17:27:47 -0300

    Edição do Codigo CCS e JS

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
+var nombre = document.querySelector('#nome');
+var correo = document.getElementById('email');
+var texto = document.getElementById('text');
+var boton = document.getElementById('buttonEnviar');
+nombre.innerHTML ="Prueba";
 function fechar() {
 	document.getElementById("check").checked = false;
 }
+
+
