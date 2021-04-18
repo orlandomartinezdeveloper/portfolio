48ea6b90de83bf4c567815da2c2b9e99620c58eb
Orlando Martinez <omjuniormusic@gmail.com>
2021-04-18 20:56:18 -0300

    Codificando JS

--- code.js
+++ code.js
@@ -1,6 +1,40 @@
-function fechar() {
-	document.getElementById("check").checked = false;
+// Declaração de Variáveis
+var linkHome = document.getElementById('linkHome').addEventListener('click', margenHome);
+var linkFormacao = document.getElementById('linkMenuForma').addEventListener('click', margenForma);
+var linkExperiencia = document.getElementById('linkMenuExp').addEventListener('click', margenExp);
+var linkPro = document.getElementById('linkMenuPro').addEventListener('click', margenPro);
+var linkFormacaoP = document.getElementById('formacao');
+var linkExperiencia = document.getElementById('experiencia');
+var linkProjetos = document.getElementById('projetos');
+
+// Funções para a margem superior em relação ao menu fixo
+function margenHome(){
+	linkFormacaoP.style.paddingTop = "0";
+	linkExperiencia.style.paddingTop = "0";
+	linkProjetos.style.paddingTop = "0";
 }
+function margenForma(){
+	linkFormacaoP.style.paddingTop = "100px";
+	linkExperiencia.style.paddingTop = "0";
+	linkProjetos.style.paddingTop = "0";
+}
+function margenExp(){
+	linkFormacaoP.style.paddingTop = "0";
+	linkExperiencia.style.paddingTop = "100px";
+	linkProjetos.style.paddingTop = "0";
+}
+function margenPro(){
+	linkFormacaoP.style.paddingTop = "0";
+	linkExperiencia.style.paddingTop = "0";
+	linkProjetos.style.paddingTop = "100px";
+}
+function margenContato(){
+	linkFormacaoP.style.paddingTop = "0";
+	linkExperiencia.style.paddingTop = "0";
+	linkProjetos.style.paddingTop = "0";
+}
+
+// Validação do Formulário
 var boton = document.getElementById('buttonEnviar').addEventListener('click', validar);
 function validar() {
 	let nombre = document.getElementById("nome");
@@ -13,3 +47,7 @@ function validar() {
 	}
 }
 
+// Função para fechar o menu mobile
+function fechar() {
+	document.getElementById("check").checked = false;
+}
